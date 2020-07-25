8f5ebaff3cc53f6a76ba1dbc514cc7eff7a80fc5
sunkai <sunkai.mr@qq.com>
2020-07-26 02:02:24 +0800

    常新插座-不带电量计量

--- plug2.0/app/include/user_led.h
+++ plug2.0/app/include/user_led.h
@@ -46,8 +46,8 @@ IS_JI_ZHI_YUN:
 */
 
 #define IS_PHILIPS      	0
-#define IS_CHANG_XIN    	1
-#define IS_CHANG_XIN_V1    	0
+#define IS_CHANG_XIN    	0
+#define IS_CHANG_XIN_V1    	1
 
 
 typedef enum tagLED_WIFI_STATUS
